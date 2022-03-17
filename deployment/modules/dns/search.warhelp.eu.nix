c99020e497c0f946ce84590e8be2f7b05f3d718d
Johannes Lötzsch <git@johannesloetzsch.de>
2022-03-17 12:03:49 +0100

    deployment: fixed AAAA record of search.warhelp.eu

--- deployment/modules/dns/search.warhelp.eu.nix
+++ deployment/modules/dns/search.warhelp.eu.nix
@@ -5,7 +5,7 @@ with dns.lib.combinators; {
     nameServer = "ns1.broenradio.org.";
 
     adminEmail = "dns-admin@warhelp.eu";
-    serial = 2022032201;
+    serial = 2022031701;
   };
 
   NS = [
@@ -15,20 +15,20 @@ with dns.lib.combinators; {
 
   #CNAME = [ "eve.thalheim.io" ];  ## not possible for the subdomain
   A = [ "88.99.244.96" ];
-  AAAA = [ "2a01:4f8:10b:49f::2" ];
+  AAAA = [ "2a01:4f8:10b:49f:1::1" ];
 
   subdomains = rec {
     #mic1 = cname "eve.thalheim.io";  ## TODO why doesn't it work?
-    mic1 = host "88.99.244.96" "2a01:4f8:10b:49f::2";
+    mic1 = host "88.99.244.96" "2a01:4f8:10b:49f:1::1";
 
     backend = mic1;
     search = mic1;
-    submission = mic1;
+    #submission = mic1;
 
-    binarycache = mic1;
-    grafana = mic1;
+    #binarycache = mic1;
+    #grafana = mic1;
 
-    prometheus-mic1 = mic1;
-    loki-mic1 = mic1;
+    #prometheus-mic1 = mic1;
+    #loki-mic1 = mic1;
   };
 }
