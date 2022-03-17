4cfd0a4b301454b5990c41e7f259a1da942e88eb
Johannes Lötzsch <git@johannesloetzsch.de>
2022-03-17 17:18:08 +0100

    backend: comment for generating new password+hash

--- backend/src/beherbergung/auth/password/hash.clj
+++ backend/src/beherbergung/auth/password/hash.clj
@@ -11,3 +11,9 @@
       false
       (let [algo :argon2]
            (verify-with algo password pwhash))))
+
+
+(comment
+  (require '[beherbergung.auth.password.hash :refer [hash-password]])
+  (require '[beherbergung.auth.password.generate :refer [generate-password]])
+  (let [pw (generate-password)] (prn (hash-password pw) pw)))
