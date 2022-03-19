ac87ac0f8bc260d56d39dca638f18957ee3d707e
Jörg Thalheim <joerg@thalheim.io>
2022-03-19 10:11:02 +0100

    allow import-from-derivation because of yarn2nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   nixConfig.extra-substituters = ["https://cache.garnix.io"];
   nixConfig.extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  nixConfig.allow-import-from-derivation = true;
 
   inputs = {
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
