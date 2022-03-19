5beb3d5fe3ca2daec9d6a37aa907080075292062
Johannes Lötzsch <git@johannesloetzsch.de>
2022-03-19 01:02:17 +0100

    backend: columns/warhelp/offers.edn

--- backend/src/beherbergung/resolver/root/ngo/get_columns.clj
+++ backend/src/beherbergung/resolver/root/ngo/get_columns.clj
@@ -7,6 +7,15 @@
             [beherbergung.model.columns :as columns :refer [->graphql]]
             [clojure.edn]))
 
+(defn query->definition:file
+  "This function can be modified to serve arbitrary column definitions"
+  [ngo:id _opt]
+  (let [ngo:name (cond (re-matches #"lifeline.*" ngo:id)
+                         "lifeline"
+                       (re-matches #"warhelp.*" ngo:id)
+                         "warhelp")]
+       (str "./data/config/columns/" ngo:name "/offers.edn")))
+
 (s/fdef get_columns
         :args (s/tuple map? (s/keys :req-un [::auth/auth]) map? map?)
         :ret (s/nilable (s/* ::columns/columns)))
@@ -14,8 +23,9 @@
 (defn get_columns
   [_node opt ctx _info]
   (let [;{:keys []} (:db_ctx ctx)
-        [ngo:id] (auth+role->entity ctx (:auth opt) ::ngo/record)]
-       (when ngo:id
-         (map ->graphql (clojure.edn/read-string (slurp "./data/config/columns/lifeline/offers.edn"))))))
+        [ngo:id] (auth+role->entity ctx (:auth opt) ::ngo/record)
+        file (query->definition:file ngo:id opt)]
+       (when (and ngo:id file)
+         (map ->graphql (clojure.edn/read-string (slurp file))))))
 
 (s/def ::get_columns (t/resolver #'get_columns))
