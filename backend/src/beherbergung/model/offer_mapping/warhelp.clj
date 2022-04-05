ef36fd73c69e2f97ff15587c080f420e21eec21e
Johannes Lötzsch <git@johannesloetzsch.de>
2022-04-05 15:25:09 +0200

    warhelp: re-matches any kind of `occupied`

--- backend/src/beherbergung/model/offer_mapping/warhelp.clj
+++ backend/src/beherbergung/model/offer_mapping/warhelp.clj
@@ -26,7 +26,7 @@
               :time_submission_str "Timestamp"
               :editor "Bearbeiter*in"
               :rw_contacted #(boolean (not-empty (get % "Kontaktiert (Name & Datum)")))
-              ;:rw_offer_occupied ["Occupied" not-empty?]
+              :rw_offer_occupied #(boolean (re-matches #"(?i).*occupied.*" %))
               :rw_note "Note"
 
               :time_from_str "Available from- / Verfügbar von- "
